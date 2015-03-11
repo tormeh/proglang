9dbe936363ce9e65f5a01fd2988aa131d573dce5
tormeh <tormod.hellen@gmail.com>
2015-03-11 15:20:05 +0100

    added scanner support for synchronized variables

--- compiler/src/main/scala/Scanner.scala
+++ compiler/src/main/scala/Scanner.scala
@@ -44,6 +44,7 @@ object FumurtScanner extends RegexParsers /*with Parsers*/
   def programStrParser: Parser[ProgramT] = positioned( new Regex("program ") ^^ {x => println("scanned program "+x.toString);ProgramT()} )
   def functionParser: Parser[FunctionT] = positioned( new Regex("function ") ^^ {x => println("scanned function "+x.toString);FunctionT()} )
   def unsafeActionParser: Parser[UnsafeActionT] = positioned( new Regex("unsafe action ") ^^ {x => println("scanned unsafe action "+x.toString);UnsafeActionT()} )
+  def synchronizedVariableParser: Parser[SynchronizedVariableT] = positioned(new Regex("synchronized variable ") ^^ {x => println("scanned synchronized variable "+x.toString); SynchronizedVariableT()})
   def valueParser: Parser[ValueT] = positioned( new Regex("value ") ^^ {x => println("scanned unsafe value "+x.toString);ValueT()} )
   def actionParser: Parser[ActionT] = positioned( new Regex("action ") ^^ {x => println("scanned action "+x.toString);ActionT()} )
   def trueParser: Parser[TrueT] = positioned( new Regex("true") ^^ {x => println("scanned true "+x.toString);TrueT()} )
@@ -71,6 +72,7 @@ object FumurtScanner extends RegexParsers /*with Parsers*/
       programStrParser          |
       unsafeActionParser        |
       actionParser              |
+      synchronizedVariableParser  |
       functionParser            |
       trueParser                |
       falseParser               |
@@ -108,6 +110,7 @@ case class ActionT() extends DefDescriptionT {override def toString = "action"}
 case class UnsafeActionT() extends DefDescriptionT {override def toString = "unsafe action"}
 case class FunctionT() extends DefDescriptionT {override def toString = "function"}
 case class ValueT() extends DefDescriptionT {override def toString = "value"}
+case class SynchronizedVariableT() extends DefDescriptionT {override def toString = "synchronized variable"}
 case class OpenParenthesisT() extends SyntaxT {override def toString = "\"(\""}
 case class CloseParenthesisT() extends SyntaxT {override def toString = "\")\""}
 case class OpenCurlyBracketT() extends SyntaxT {override def toString = "\"{\""}
