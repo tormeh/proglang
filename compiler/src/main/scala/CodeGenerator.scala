ba4d23a2992a4858795f6aa2734e78fdf8882421
Tormod Hellen <tormod.hellen@gmail.com>
2015-03-16 23:46:29 +0100

    code generation

--- compiler/src/main/scala/CodeGenerator.scala
+++ compiler/src/main/scala/CodeGenerator.scala
@@ -6,7 +6,7 @@ object FumurtCodeGenerator
   def generate(ast:List[Definition]):String =
   {
     val includestatement = "#include <iostream>\n#include <thread>\n#include <string>\n#include <atomic>\n#include <condition_variable>\n#include <list>\n#include <chrono>\n\n\n"
-    val topthreads = getthreads(ast)
+    val topthreads = gettopthreadstatements(ast)
     val numtopthreads = topthreads.length
     val synchronizationGlobalVars = "static std::atomic<int> rendezvousCounter;\nstatic std::mutex rendezvousSyncMutex;\nstatic std::condition_variable cv;"
     val main = getmain(topthreads)
@@ -14,7 +14,7 @@ object FumurtCodeGenerator
     includestatement + "#define NUMTOPTHREADS " + numtopthreads.toString + "\n" + synchronizationGlobalVars + "\n\n" + main
   }
   
-  def getthreads(ast:List[Definition]):List[FunctionCallStatement]=
+  def gettopthreadstatements(ast:List[Definition]):List[FunctionCallStatement]=
   {
     ast.find(x => (x.leftside.description match {case ProgramT() => true; case _=> false})) match
     {
@@ -33,12 +33,19 @@ object FumurtCodeGenerator
   def getmain(topthreads:List[FunctionCallStatement]):String =
   {
     var threadsStart = ""
+    var counter = 0
     
     for(i<-topthreads)
     {
-      threadsStart = threadsStart + "\n" + "std::thread t1 (" + i.functionidentifier + ");"
+      threadsStart = threadsStart + "\n" + "std::thread t" + counter + " (" + i.functionidentifier + ");"
+      counter+=1
     }
     
-    "int main()\n{\nrendezvousCounter.store(0);" + threadsStart + "\n}"
+    "int main()\n{\nrendezvousCounter.store(0);" + threadsStart + "\nwhile(true)\n{\nstd::this_thread::sleep_for(std::chrono::seconds(1));\n}" + "\n}"
+  }
+  
+  def gettopthtreadstrings(ast):String =
+  {
+    val topthreads = ast.filter(x => x.leftside.description match {case ThreadT() => true; case _=> false;})
   }
 }
