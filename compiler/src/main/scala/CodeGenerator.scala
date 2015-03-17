f74d946213356ae2424bb770c222528ff6cb6365
tormeh <tormod.hellen@gmail.com>
2015-03-17 16:59:46 +0100

    code generator

--- compiler/src/main/scala/CodeGenerator.scala
+++ compiler/src/main/scala/CodeGenerator.scala
@@ -10,8 +10,11 @@ object FumurtCodeGenerator
     val numtopthreads = topthreads.length
     val synchronizationGlobalVars = "static std::atomic<int> rendezvousCounter;\nstatic std::mutex rendezvousSyncMutex;\nstatic std::condition_variable cv;"
     val main = getmain(topthreads)
+    val synchvars = getsynchronizedvariables(ast)
+    val syncfunc = getsynchonizerfunction(synchvars)
+    val synchvardeclarations = getGlobalSynchVariableDeclarations(synchvars)
     
-    includestatement + "#define NUMTOPTHREADS " + numtopthreads.toString + "\n" + synchronizationGlobalVars + "\n\n" + main
+    includestatement + synchvardeclarations + "#define NUMTOPTHREADS " + numtopthreads.toString + "\n" + synchronizationGlobalVars + syncfunc + "\n\n" + main
   }
   
   def getthreads(ast:List[Definition]):List[FunctionCallStatement]=
@@ -33,12 +36,90 @@ object FumurtCodeGenerator
   def getmain(topthreads:List[FunctionCallStatement]):String =
   {
     var threadsStart = ""
+    var counter = 0
     
     for(i<-topthreads)
     {
-      threadsStart = threadsStart + "\n" + "std::thread t1 (" + i.functionidentifier + ");"
+      threadsStart = threadsStart + "\n" + "std::thread t"+counter+" (" + i.functionidentifier + ");"
+      counter+=1
     }
     
-    "int main()\n{\nrendezvousCounter.store(0);" + threadsStart + "\n}"
+    "int main()\n{\nrendezvousCounter.store(0);" + threadsStart + "while(true)\n{\nstd::this_thread::sleep_for(std::chrono::seconds(1));\n}" + "\n}"
   }
+  
+  def getsynchonizerfunction(synchvariables:List[Definition]):String=
+  {
+    var synchvariablestrings = ""
+    
+    for(i<-synchvariables)
+    {
+      val name = i.leftside.id.value
+      synchvariablestrings += name + " = write" + name.capitalize + ";\n"
+      
+    }
+    
+    """static void waitForRendezvous(std::string name)
+{
+  std::unique_lock<std::mutex> lk(rendezvousSyncMutex);
+  ++rendezvousCounter;
+  if(rendezvousCounter.load() < NUMTOPTHREADS)
+  {
+    cv.wait(lk);
+  }
+  else if (rendezvousCounter.load() == NUMTOPTHREADS)
+  {
+    while(!print1.empty())
+    {
+      std::cout << print1.front();
+      print1.pop_front();
+    }
+    while(!print2.empty())
+    {
+      std::cout << print2.front();
+      print2.pop_front();
+    }
+    """ + synchvariablestrings + """
+    {
+      rendezvousCounter.store(0);
+      cv.notify_all();
+    }
+  }
+  else
+  {
+    std::cout << "error in wait for " << name << ". Rendezvouscounter out of bounds. RedezvousCounter = " << rendezvousCounter.load() << "\n";
+    exit(0);
+  }
+}"""
+  }
+  
+  def getGlobalSynchVariableDeclarations(synchvariables:List[Definition]):String=
+  {
+    var synchdeclares = ""
+    for(i<-synchvariables)
+    {
+      val fumurttype = i.leftside.returntype.value
+      if (fumurttype == "Integer") 
+      {
+        synchdeclares += "\nstatic int " + i.leftside.id.value + " = " + i.rightside.expressions(0).args      //TODO
+      }
+    }
+    synchdeclares
+  }
+  
+  def getsynchronizedvariables(ast:List[Definition]):List[Definition]=
+  {
+    ast.find(x => (x.leftside.description match {case ProgramT() => true; case _=> false})) match
+    {
+      case None => println("Error in getthreads. Should be caught by checker."); scala.sys.exit()
+      case Some(res) => 
+      {
+        res.rightside.expressions.flatMap(x => x match
+        {
+          case x:Definition => x.leftside.description match {case SynchronizedVariableT() => Some(x); case _=> None}
+          case _=> None
+        })
+      }
+    }
+  }
+  
 }
